abcd101ce2e3808a29d52c8b292253b0d8fe50b3
Sedryiqbal <sendrym94@gmail.com>
2023-10-07 10:58:02 +0700

    Belajar CRUD golang

---
+++ config/database.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DBConnetion() (*sql.DB, error) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := ""
+	dbName := "go-crud"
+
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	return db, err
+}
